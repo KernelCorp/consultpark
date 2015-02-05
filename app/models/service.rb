7f2c9dfc5ba7e42ed7a800338da3df1dbc5b8687
Zamyatin-AA <stekbox@gmail.com>
2015-02-05 13:08:37 +0600

    Table fixed, sorting fixed

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,9 +4,10 @@ class Service
 
   field :title, type: String
   field :price, type: Integer
+  field :position
 
   belongs_to :category
 
-  default_scope -> {desc :position_field}
+  default_scope -> {asc :position}
 
 end
